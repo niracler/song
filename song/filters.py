38bb3eeb57a91f5a64d63656c289aa87db4cef7b
Niracler <1026037967@qq.com>
2019-11-18 21:03:35 +0800

    fix sid

--- song/filters.py
+++ song/filters.py
@@ -4,12 +4,12 @@ from .models import Song
 
 class SongFiliter(django_filters.rest_framework.FilterSet):
     """电歌曲的api的过滤器"""
-    mid = django_filters.CharFilter(field_name='mid')
+    sid = django_filters.CharFilter(field_name='sid')
     name = django_filters.CharFilter(field_name='name')
     created = django_filters.CharFilter(field_name='created')
 
     class Meta:
         model = Song
         fields = [
-            'mid', 'name', 'created'
+            'sid', 'name', 'created'
         ]
