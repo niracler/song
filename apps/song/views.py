e53ff88a4778219e0705b2e9b423ddbb59b2463c
niracler <niracler@gmail.com>
2019-11-26 08:50:06 +0800

    ps

--- apps/song/views.py
+++ apps/song/views.py
@@ -18,8 +18,8 @@ from rest_framework_extensions.cache.mixins import CacheResponseMixin
 class Pagination(PageNumberPagination):
     """用于内容分页的类"""
     page_size = 10
-    page_size_query_param = 'pageSize'
-    page_query_param = 'page'
+    page_size_query_param = 'ps'
+    page_query_param = 'p'
     max_page_size = 300
 
 
